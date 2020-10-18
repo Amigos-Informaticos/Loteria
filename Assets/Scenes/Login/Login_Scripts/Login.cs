f35aabe01b41b3369b912d1b586aa6e5cf05f246
Edson Manuel Carballo Vera <edsonmanuelcarballovera@gmail.com>
2020-10-18 16:47:58 -0500

    Refactorización de clases Board y Player a inglés
    Modifiqué las clases para que todas las variables estén en inglés, además de optimizar algunas pequeñas cosas.

--- Assets/Scenes/Login/Login_Scripts/Login.cs
+++ Assets/Scenes/Login/Login_Scripts/Login.cs
@@ -5,36 +5,33 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-public class Login : MonoBehaviour
-{
-    
-    [SerializeField] private InputField userNameInput = null;
-    [SerializeField] private InputField emailInput = null;
-    [SerializeField] private InputField passwordInput = null;
+public class Login : MonoBehaviour {
+	[SerializeField] private InputField userNameInput = null;
+	[SerializeField] private InputField emailInput = null;
+	[SerializeField] private InputField passwordInput = null;
 
-    public void CreateUser(Action<Response> response)
-    {
-        StartCoroutine(CO_CreateUser(userNameInput.text, emailInput.text, passwordInput.text, response));
-    } 
-    
-    private IEnumerator CO_CreateUser(string userName, string email, string password, Action<Response> response){
-        WWWForm form = new WWWForm();
-        form.AddField("userName", userName);
-        form.AddField("email", email);
-        form.AddField("password", password);
-        
-        WWW connection = new WWW("http://localhost/Game/createUser.php", form);
+	public void CreateUser(Action<Response> response) {
+		StartCoroutine(CO_CreateUser(userNameInput.text, emailInput.text, passwordInput.text,
+			response));
+	}
 
-        yield return connection;
+	private IEnumerator CO_CreateUser(string userName, string email, string password,
+	                                  Action<Response> response) {
+		WWWForm form = new WWWForm();
+		form.AddField("userName", userName);
+		form.AddField("email", email);
+		form.AddField("password", password);
 
-        response(JsonUtility.FromJson<Response>(connection.text));
-    }
-    
-    
+		WWW connection = new WWW("http://localhost/Game/createUser.php", form);
+
+		yield return connection;
+
+		response(JsonUtility.FromJson<Response>(connection.text));
+	}
 }
+
 [Serializable]
-public class Response
-{
-    public bool done = false;
-    public string message = "";
+public class Response {
+	public bool done = false;
+	public string message = "";
 }
