10f354fd03f41cbae11e041269a60eb66cf9bf4a
Edson Manuel Carballo Vera <edsonmanuelcarballovera@gmail.com>
2020-11-09 20:32:06 -0600

    Correciones otra vez

--- Assets/Logic/Connection/TCPSocket.cs
+++ Assets/Logic/Connection/TCPSocket.cs
@@ -3,27 +3,34 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 public class TCPSocket {
 	public string Server { get; set; }
 	public int Port { get; set; }
-	public List<string> Messages { get; set; }
+	public List<Command> Messages { get; set; }
 	private TcpClient _client;
 	private NetworkStream _stream;
 	private const int MAXTIMEOUT = 750;
+	private Thread _thread;
+	private string response;
 
 	public TCPSocket(string server, int port) {
 		this.Server = server;
 		this.Port = port;
-		this.Messages = new List<string>();
+		this.Messages = new List<Command>();
 	}
 
 	private void Prepare() {
-		this._client ??= new TcpClient(this.Server, this.Port);
+		if (this._client == null) {
+			this._client = new TcpClient(this.Server, this.Port);
+		}
 		if (!this._client.Connected) {
 			this._client.Connect(this.Server, this.Port);
 		}
-		this._stream ??= this._client.GetStream();
+		if (this._stream == null) {
+			this._stream = this._client.GetStream();
+		}
 	}
 
 	public void Close() {
@@ -31,30 +38,35 @@ public class TCPSocket {
 		this._client.Close();
 	}
 
-	public void AddMessage(string message) {
-		this.Messages.Add(message);
+	public void AddCommand(Command command) {
+		this.Messages.Add(command);
 	}
 
-	public bool SendMessage(Command command) => this.SendMessage(command.GetJSON());
+	public void SendCommand() {
+		this._thread = new Thread(this.SendAndGet);
+		this._thread.Start();
+	}
 
-	public bool SendMessage(string message = null) {
-		bool sent = false;
-		if (this.Messages.Count <= 0 && message == null) return false;
-		if (message != null) {
-			if (this.Messages.Count > 0) {
-				this.Messages.Insert(0, message);
-			} else {
-				this.Messages.Add(message);
-			}
+	private void SendAndGet() {
+		Thread newThread = new Thread(this.Send);
+		newThread.Start();
+		newThread.Join();
+		newThread = new Thread(this.Read);
+		newThread.Start();
+		newThread.Join();
+	}
+
+	public void Send() {
+		if (this.Messages.Count == 0) {
+			return;
 		}
 		try {
 			this.Prepare();
-			byte[] data = Encoding.UTF8.GetBytes(this.Messages[0]);
+			byte[] data = Encoding.UTF8.GetBytes(this.Messages[0].GetJSON());
 			try {
 				this._stream.Write(data, 0, data.Length);
 
 				this.Messages.RemoveAt(0);
-				sent = true;
 			}
 			catch (Exception e) {
 				Console.Error.Write(e.Message);
@@ -63,10 +75,18 @@ public class TCPSocket {
 		catch (Exception e) {
 			Console.Error.Write(e.Message);
 		}
-		return sent;
 	}
 
-	public string Read() {
+	public string GetResponse() {
+		string response = null;
+		if (this._thread != null) {
+			this._thread.Join();
+			response = this.response;
+		}
+		return response;
+	}
+
+	public void Read() {
 		byte[] received = new byte[1024];
 		string response = "NO RESPONSE";
 		int tamanio = 0;
@@ -82,12 +102,10 @@ public class TCPSocket {
 		catch (Exception e) {
 			Console.Error.Write(e.Message);
 		}
-		return response;
+		this.response = response;
 	}
 
-	public string Chat(Command command) => this.Chat(command.GetJSON());
-
-	public string Chat(string message = null) {
+	public string Chat(Command message = null) {
 		string response = "NO RESPONSE";
 		if (this.Messages.Count <= 0 && message == null) return null;
 		if (message != null) {
@@ -99,7 +117,7 @@ public class TCPSocket {
 		}
 		try {
 			this.Prepare();
-			byte[] data = Encoding.UTF8.GetBytes(this.Messages[0]);
+			byte[] data = Encoding.UTF8.GetBytes(this.Messages[0].GetJSON());
 			try {
 				this._stream.Write(data, 0, data.Length);
 				this.Messages.RemoveAt(0);
