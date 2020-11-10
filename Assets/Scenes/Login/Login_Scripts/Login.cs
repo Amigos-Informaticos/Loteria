f50beb410d7b1ec29431d271177d19d2ec4c8ed4
Efrain Arenas <efrain_escuela@outlook.com>
2020-11-09 18:14:55 -0600

    Prueba de ingreso de datos por GUI

--- Assets/Scenes/Login/Login_Scripts/Login.cs
+++ Assets/Scenes/Login/Login_Scripts/Login.cs
@@ -3,31 +3,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 
 public class Login : MonoBehaviour {
-	[SerializeField] private InputField userNameInput = null;
-	[SerializeField] private InputField emailInput = null;
-	[SerializeField] private InputField passwordInput = null;
 
+	public TextMeshProUGUI ingresoEmail;
+	public TextMeshProUGUI ingresoContrasenia;
+	
 	public void CreateUser(Action<Response> response) {
-		StartCoroutine(CO_CreateUser(userNameInput.text, emailInput.text, passwordInput.text,
-			response));
+		Command command = new Command("save_user");
+		command.AddArgument("name", "efrain");
+		command.AddArgument("last_name", "arenas");
+		command.AddArgument("nickname", "efraincami");
+		command.AddArgument("email", "efrain@correo.com");
+		command.AddArgument("password", "beethoven");
+
+		TCPSocket tcp = new TCPSocket("201.105.200.72", 42069);
+		//string salida = tcp.Chat(command);
+
+		//Console.WriteLine(salida);
 	}
 
-	private IEnumerator CO_CreateUser(string userName, string email, string password,
-	                                  Action<Response> response) {
-		WWWForm form = new WWWForm();
-		form.AddField("userName", userName);
-		form.AddField("email", email);
-		form.AddField("password", password);
+	public void ProbarInput()
+	{
+		string email = ingresoEmail.text;
+		string password = ingresoContrasenia.text;
 
-		WWW connection = new WWW("http://localhost/Game/createUser.php", form);
-
-		yield return connection;
-
-		response(JsonUtility.FromJson<Response>(connection.text));
+		Debug.Log(email + " " +  password);
 	}
+
+	
 }
 
 [Serializable]
