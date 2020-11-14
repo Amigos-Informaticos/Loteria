bcc30fd5aef44c89a413e355e215619c3a1b0e52
Edson Manuel Carballo Vera <edsonmanuelcarballovera@gmail.com>
2020-11-14 11:43:28 -0600

    Cambios de namespace de clases de conexión

--- Assets/Logic/Connection/Configuration/TCPSocketConfiguration.cs
+++ Assets/Logic/Connection/Configuration/TCPSocketConfiguration.cs
@@ -1,15 +1,12 @@
-namespace Logic.Connection.Configuration {
-	using System.Net;
-	using TCPSocketClient;
+using System.Net;
 
-	public abstract class TCPSocketConfiguration {
-		public static void BuildDefaultConfiguration(out TCPSocket socket) {
-			socket = null;
-			using WebClient client = new WebClient();
-			string address =
-				client.DownloadString("https://amigosinformaticos.000webhostapp.com/");
-			string serverAddress = address == "201.105.200.72" ? "192.168.1.99" : "201.105.200.72";
-			socket = new TCPSocket(serverAddress, 42069);
-		}
+public abstract class TCPSocketConfiguration {
+	public static void BuildDefaultConfiguration(out TCPSocket socket) {
+		socket = null;
+		using WebClient client = new WebClient();
+		string address =
+			client.DownloadString("https://amigosinformaticos.000webhostapp.com/");
+		string serverAddress = address == "201.105.200.72" ? "192.168.1.99" : "201.105.200.72";
+		socket = new TCPSocket(serverAddress, 42069);
 	}
 }
