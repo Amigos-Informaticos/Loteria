927fee5888bf955494b70ad6d1ae48c8e5739d4d
Alexis Alvarez <alexisao@hotmail.com>
2020-11-15 02:11:26 -0600

    Validaciones en el modelo Player
    También se agregaron algunas pruebas unitarias de este

--- Assets/Logic/Model/Player.cs
+++ Assets/Logic/Model/Player.cs
@@ -1,11 +1,99 @@
+using System;
+using System.Text.RegularExpressions;
+
 public class Player {
-	public int Score { get; set; }
+	private string names;
+	private string lastName;
+	private string email;
+	public string Names
+    {
+        get
+        {
+			return this.names;
+        }
+        set
+        {            
+            if (Player.IsName(value))
+            {
+				this.names = value;
+			}
+            else
+            {
+				this.names = null;
+            }
+			
+		}
+    }
+
+	public string LastName
+    {
+        get
+        {
+			return this.lastName;
+        }
+        set
+        {
+            if (Player.IsName(value))
+            {
+				this.lastName = value;
+            }
+            else
+            {
+				this.lastName = null;
+            }
+        }
+    }
+
+	public string Email
+    {
+        get
+        {
+			return this.email;
+        }
+		set
+        {
+			if (Player.IsEmail(value))
+			{
+				this.email = value;
+			}
+            else
+            {
+				this.email = null;
+            }
+        }
+    }
+
+    public int Score { get; set; }
 	public Board Board { get; set; }
 
 	public Player() {
 		this.Score = 0;
 		this.Board = new Board();
 	}
+
+	public static bool IsName(string names)
+	{
+		bool isName = false;
+		Regex regex = new Regex("^[A-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$");
+		if (regex.IsMatch(names))
+		{
+			isName = true;
+		}
+		
+		return isName;
+	}
+
+	public static bool IsEmail(string email)
+    {
+		bool isEmail = false;
+		Regex regex = new Regex("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
+        if (regex.IsMatch(email))
+        {
+			isEmail = true;
+        }
+        
+		return isEmail;
+    }
 
 	public void MakeNewBoard() {
 		this.Board = new Board();
