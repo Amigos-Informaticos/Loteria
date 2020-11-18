2ea32b2c35f04a5fedb451113935a26b59d43996
Edson Manuel Carballo Vera <edsonmanuelcarballovera@gmail.com>
2020-11-18 02:51:19 -0600

    Registro de configuración de usuario funcional Prueba pasada

---
+++ Assets/Tests/UserSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace Tests
+{
+	using System.IO;
+	using NUnit.Framework;
+
+	public class UserSettings
+	{
+		[Test]
+		public void UserSettingsSimplePasses()
+		{
+			UserConfiguration.SaveSettings();
+			string path = "Assets/Resources/UserConfigurationFiles/UserConfiguration.json";
+			Assert.IsTrue(File.Exists(path));
+		}
+	}
+}
