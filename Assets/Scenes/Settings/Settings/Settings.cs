f88a309aa5f7bf6d1a1774cc2f37889be263c702
Efrain Arenas <efrain_escuela@outlook.com>
2020-11-19 17:07:24 -0600

    Creación de SignedIn

---
+++ Assets/Scenes/Settings/Settings/Settings.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    public void BackToMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(3);
+    }
+
+    public void SaveChanges()
+    {
+        
+    }
+}
