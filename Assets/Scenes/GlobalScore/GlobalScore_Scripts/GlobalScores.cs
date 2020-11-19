6ee9ef3de9567334341c346a50617127690de676
Efrain Arenas <efrain_escuela@outlook.com>
2020-11-18 21:57:57 -0600

    Score Con top 10 generado automáticamente

--- Assets/Scenes/GlobalScore/GlobalScore_Scripts/GlobalScores.cs
+++ Assets/Scenes/GlobalScore/GlobalScore_Scripts/GlobalScores.cs
@@ -5,26 +5,25 @@ using UnityEngine;
 
 public class GlobalScores : MonoBehaviour
 {
-    private Transform entryContainer;
-    private Transform entryTemplate;
+    public GameObject entryContainer;
+    public GameObject entryTemplate;
     
     private void Awake()
     {
-        entryContainer = transform.Find("HighScoreEntryContainer");
-        entryTemplate = entryContainer.Find("HighScoreEntryTemplate");
+        //entryContainer = transform.Find("HighScoreEntryContainer");
+        //entryTemplate = entryContainer.Find("HighScoreEntryTemplate");
         
         entryTemplate.gameObject.SetActive(false);
-
-
-        float templateHeight = 20f;
+        
+        float templateHeight = 40f;
         for (int i = 0; i < 10; i++)
         {
-            Transform entryTransform = Instantiate(entryTemplate, entryContainer);
+            GameObject entryTransform = Instantiate(entryTemplate, entryContainer.transform, true);
             RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
             entryRectTransform.anchoredPosition = new Vector2(0,-templateHeight * i);
+            //entryTransform.Find("Place").GetComponent<TextMeshProUGUI>().text = (i + 1).ToString();
+            entryTransform.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = (i + 1).ToString();
             entryTransform.gameObject.SetActive(true);
         }
     }
-
-    
 }
