d8146b02f60014e470d750dd4e03b96691f4ff2e
Efrain Arenas <efrain_escuela@outlook.com>
2020-11-18 23:59:56 -0600

    Probando Sign Up con la refactorización

--- Assets/Scenes/GlobalScore/GlobalScore_Scripts/GlobalScores.cs
+++ Assets/Scenes/GlobalScore/GlobalScore_Scripts/GlobalScores.cs
@@ -1,18 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class GlobalScores : MonoBehaviour
 {
-    public GameObject entryContainer;
-    public GameObject entryTemplate;
-    
+    [SerializeField] private GameObject entryContainer;
+    [SerializeField] private GameObject entryTemplate;
+
     private void Awake()
     {
-        //entryContainer = transform.Find("HighScoreEntryContainer");
-        //entryTemplate = entryContainer.Find("HighScoreEntryTemplate");
-        
         entryTemplate.gameObject.SetActive(false);
         
         float templateHeight = 40f;
@@ -21,7 +19,6 @@ public class GlobalScores : MonoBehaviour
             GameObject entryTransform = Instantiate(entryTemplate, entryContainer.transform, true);
             RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
             entryRectTransform.anchoredPosition = new Vector2(0,-templateHeight * i);
-            //entryTransform.Find("Place").GetComponent<TextMeshProUGUI>().text = (i + 1).ToString();
             entryTransform.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = (i + 1).ToString();
             entryTransform.gameObject.SetActive(true);
         }
