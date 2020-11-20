735bf8c5c986f5ef2a37fbe4f2fedb8c1f70477f
Edson Manuel Carballo Vera <edsonmanuelcarballovera@gmail.com>
2020-11-19 23:16:40 -0600

    Solucion de lenguage hard coded

--- Assets/Scenes/Login/Login_Scripts/Login.cs
+++ Assets/Scenes/Login/Login_Scripts/Login.cs
@@ -14,7 +14,6 @@ public class Login : MonoBehaviour
 
 	private void Start()
 	{
-		Localization.Language = "English";
 		this.emailPlaceHolder.text = Localization.GetMessage("Login", "Email");
 		this.passwordPlaceHolder.text = Localization.GetMessage("Login", "Password");
 		this.backButton.text = Localization.GetMessage("Login", "Back");
