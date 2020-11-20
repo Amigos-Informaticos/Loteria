6248e653a06351ab81f207eea55238449f2ef16d
Efrain Arenas <efrain_escuela@outlook.com>
2020-11-19 18:04:31 -0600

    Controlador de Settings

--- Assets/Scenes/Settings/Settings/Settings.cs
+++ Assets/Scenes/Settings/Settings/Settings.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Settings : MonoBehaviour
 {
-    
+    [SerializeField] private Transform languageDD;
+    [SerializeField] private Text languageLabel;
     void Start()
     {
-        
+        UserConfiguration.LoadSettings();
+        int menuIndex = languageDD.GetComponent<Dropdown>().value;
+        List<Dropdown.OptionData> menuOptions = languageDD.GetComponent<Dropdown>().options;
+        languageDD.GetComponent<Dropdown>().value = 0;
     }
 
     public void BackToMenu()
@@ -15,8 +21,19 @@ public class Settings : MonoBehaviour
         UnityEngine.SceneManagement.SceneManager.LoadScene(3);
     }
 
+    public string GetLanguage()
+    {
+        int menuIndex = languageDD.GetComponent<Dropdown>().value;
+        List<Dropdown.OptionData> menuOptions = languageDD.GetComponent<Dropdown>().options;
+        string language = menuOptions[menuIndex].text;
+        return language;
+    }
+    
     public void SaveChanges()
     {
-        
+        UserConfiguration.MusicVolume = "100";
+        UserConfiguration.FXSVolume = "100";
+        UserConfiguration.Language = this.GetLanguage();
+        UserConfiguration.SaveSettings();
     }
 }
---
+++ Assets/Scenes/SignedIn/SignedInScripts/SignedIn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignedIn : MonoBehaviour
+{
+    public void GoToPlay()
+    {
+        
+    }
+
+    public void GoToScores()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(6);
+    }
+    
+    public void GoToSettings()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(5);
+    }
+
+    public void BackToMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+}
