8fe612170b1b353748235e46aa118e4498c7b4bc
Alexis Alvarez <alexisao@hotmail.com>
2020-11-21 18:23:40 -0600

    SocketConfiguration y SettingTest
    Ahora se recibe el puerto desde el archivo de configuración "SocketConfiguration"

--- Assets/Tests/SettingsTest.cs
+++ Assets/Tests/SettingsTest.cs
@@ -1,16 +1,28 @@
 namespace Tests
 {
 	using NUnit.Framework;
+    using System;
 
-	public class SettingsTest
+    public class SettingsTest
 	{
 		[Test]
-		public void SettingTest()
+		public void SettingServerAdressTest()
 		{
 			TCPSocketConfiguration.BuildDefaultConfiguration(out TCPSocket socket);
 			Assert.AreEqual(
 				socket.Server,
 				TCPSocketConfiguration.GetSetting("Address")
+			);
+		}
+		[Test]
+		public void SettingServerPortNumberTest()
+        {
+			TCPSocketConfiguration.BuildDefaultConfiguration(out TCPSocket socket);
+			string port = Convert.ToString(socket.Port);
+
+			Assert.AreEqual(
+				port,
+				TCPSocketConfiguration.GetSetting("NumberServicePort")
 			);
 		}
 	}
