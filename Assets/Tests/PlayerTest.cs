8d0c735ede3aaa143079de192af95a4bf144d794
Edson Manuel Carballo Vera <edsonmanuelcarballovera@gmail.com>
2020-11-26 16:58:39 -0600

    Corrección en diccionario de puntuaciones top ten

--- Assets/Tests/PlayerTest.cs
+++ Assets/Tests/PlayerTest.cs
@@ -8,59 +8,60 @@ using UnityEngine;
 
 namespace Tests
 {
-    class PlayerTest
-    {        
-        [Test]
-        public void LogIn()
-        {
-            string response = null;
-            Player alexito = new Player
-            {
-                Email = "alexisao@hotmail.com",
-                Password = "charlotte123"
-            };
-            response = alexito.LogIn();
-            Debug.Log(response);
-            Assert.IsTrue(response.Equals("OK"));
-        }
+	class PlayerTest
+	{
+		[Test]
+		public void LogIn()
+		{
+			string response = null;
+			Player alexito = new Player
+			                 {
+				                 Email = "alexisao@hotmail.com",
+				                 Password = "charlotte123"
+			                 };
+			response = alexito.LogIn();
+			Debug.Log(response);
+			Assert.IsTrue(response.Equals("OK"));
+		}
 
-        [Test]
-        public void SignUp()
-        {
-            Player alexito = new Player
-            {
-                Email = "alexisao@hotmail.com",
-                Password = "charlotte123",
-                NickName = "VeggieCode",
-                Names = "Alexis",
-                LastName = "Alvarez Ortega",
-                Code = "8cd6f"
-            };
-            string response = alexito.SignUp();
-            Debug.Log(response);
-            Assert.IsTrue(response.Equals("OK"));
-        }
+		[Test]
+		public void SignUp()
+		{
+			Player alexito = new Player
+			                 {
+				                 Email = "alexisao@hotmail.com",
+				                 Password = "charlotte123",
+				                 NickName = "VeggieCode",
+				                 Names = "Alexis",
+				                 LastName = "Alvarez Ortega",
+				                 Code = "8cd6f"
+			                 };
+			string response = alexito.SignUp();
+			Debug.Log(response);
+			Assert.IsTrue(response.Equals("OK"));
+		}
 
-        [Test]
-        public void SendCode()
-        {                       
-            Player alexito = new Player
-            {
-                Email = "alexisao@hotmail.com",
-                Password = "charlotte123",
-                NickName = "VeggieCode",
-                Names = "Alexis",
-                LastName = "Alvarez Ortega"
-            };
-            string response = alexito.SendCode();
-            Debug.Log(response);
-            Assert.IsTrue(response.Equals("OK"));
-        }
-        [Test]
-        public void IsName()
-        {            
-            Assert.IsTrue(Player.IsName("Efrain Razziel"));
-        }
+		[Test]
+		public void SendCode()
+		{
+			Player alexito = new Player
+			                 {
+				                 Email = "alexisao@hotmail.com",
+				                 Password = "charlotte123",
+				                 NickName = "VeggieCode",
+				                 Names = "Alexis",
+				                 LastName = "Alvarez Ortega"
+			                 };
+			string response = alexito.SendCode();
+			Debug.Log(response);
+			Assert.IsTrue(response.Equals("OK"));
+		}
+
+		[Test]
+		public void IsName()
+		{
+			Assert.IsTrue(Player.IsName("Efrain Razziel"));
+		}
 
 		[Test]
 		public void IsEmail()
