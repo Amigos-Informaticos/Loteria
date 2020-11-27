661c8c9f2ae86215de276d22415615609cbc7bd4
Efrain Arenas <efrain_escuela@outlook.com>
2020-11-26 22:49:28 -0600

    Creación dinámica del tablero
    La bbsita bblin

--- Assets/Scenes/Party/Scripts/PartyScript.cs
+++ Assets/Scenes/Party/Scripts/PartyScript.cs
@@ -6,15 +6,23 @@ using UnityEngine.UI;
 public class PartyScript : MonoBehaviour
 {
     [SerializeField] private Image[] board = new Image[25];
+    [SerializeField] private Sprite cardChoosed;
     private Player player;
+    
     // Start is called before the first frame update
     void Start()
     {
-        Sprite card;
-        card = (Sprite) Resources.Load("Assets/Images/Cards/1.jpg");
-        board[0].GetComponent<Image>().sprite = card;
-
+        this.generateBoard();
     }
+
+    public Sprite CreateSprite(int idCard)
+    {
+        Texture2D texture = Resources.Load("Images/Cards/" + idCard) as Texture2D;
+        Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+        return sprite;
+    }
+    
+    
 
     // Update is called once per frame
     void Update()
@@ -22,17 +30,14 @@ public class PartyScript : MonoBehaviour
         
     }
     
-    /*
+    
     public void generateBoard()
     {
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < 25; i++)
         {
-            for (int j = 0; j < 5; j++)
-            {
-                board[i, j].graphic.mainTexture
-            }
+            board[i].GetComponent<Image>().sprite = this.CreateSprite(i + 1);
         }
     }
-    */
+    
     
 }
