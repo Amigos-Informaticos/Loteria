20dfa2b6b39da3a80c8a8079e1b82c64cc1c7950
Edson Manuel Carballo Vera <edsonmanuelcarballovera@gmail.com>
2020-11-28 01:16:13 -0600

    Cambio de ruta en código a archivo de configuracion

--- Assets/Scenes/Party/Scripts/PartyScript.cs
+++ Assets/Scenes/Party/Scripts/PartyScript.cs
@@ -1,36 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class PartyScript : MonoBehaviour
 {
-    [SerializeField] private Image[] board = new Image[25];
-    private readonly Player player = new Player();
-    
-    void Start()
-    {
-        this.generateBoard();
-    }
+	[SerializeField] private Image[] board = new Image[25];
+	private readonly Player _player = new Player();
 
-    public Sprite CreateSprite(int idCard)
-    {
-        Texture2D texture = Resources.Load("Images/Cards/" + idCard) as Texture2D;
-        Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
-        return sprite;
-    }
+	public void Start()
+	{
+		this.generateBoard();
+	}
 
-    public void generateBoard()
-    {
-        int idBoardCard = 0;
-        for (int i = 0; i < 5; i++)
-        {
-            for (int j = 0; j < 5; j++)
-            {
-                Debug.Log(idBoardCard);
-                board[idBoardCard].GetComponent<Image>().sprite = this.CreateSprite(player.Board.Cards[i,j]);
-                idBoardCard++;
-            }
-        }
-    }
+	public Sprite CreateSprite(int idCard)
+	{
+		string path = MainConfiguration.GetSetting("CardsDirectoryPath");
+		Texture2D texture = Resources.Load(path + idCard) as Texture2D;
+		Sprite sprite = Sprite.Create(
+			texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+		return sprite;
+	}
+
+	public void generateBoard()
+	{
+		int idBoardCard = 0;
+		for (int i = 0; i < 5; i++)
+		{
+			for (int j = 0; j < 5; j++)
+			{
+				this.board[idBoardCard].GetComponent<Image>().sprite =
+					this.CreateSprite(this._player.Board.Cards[i, j]);
+				idBoardCard++;
+			}
+		}
+	}
 }
