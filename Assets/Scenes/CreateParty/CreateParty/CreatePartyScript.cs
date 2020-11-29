62d166cf6d997675530058efc39baf5caa76654c
Efrain Arenas <efrain_escuela@outlook.com>
2020-11-29 01:25:11 -0600

    Create Party Script

--- Assets/Scenes/CreateParty/CreateParty/CreatePartyScript.cs
+++ Assets/Scenes/CreateParty/CreateParty/CreatePartyScript.cs
@@ -9,14 +9,21 @@ public class CreatePartyScript : MonoBehaviour
     [SerializeField] private TextMeshProUGUI txtRounds;
     [SerializeField] private TextMeshProUGUI txtGameMode;
     [SerializeField] private TextMeshProUGUI txtSpeed;
+    [SerializeField] private TextMeshProUGUI btnCreate;
+    [SerializeField] private TextMeshProUGUI btnBack;
     
     void Start()
     {
-        
+        this.txtPlayers.text = Localization.GetMessage("CreateParty","Players");
+        this.txtRounds.text = Localization.GetMessage("CreateParty","Rounds");
+        this.txtGameMode.text = Localization.GetMessage("CreateParty","GameMode");
+        this.txtSpeed.text = Localization.GetMessage("CreateParty","Speed");
+        this.btnCreate.text = Localization.GetMessage("CreateParty","Create");
+        this.btnBack.text = Localization.GetMessage("CreateParty","Back");
     }
 
-    void Update()
+    public void BackToLetsPlay()
     {
-        
+        UnityEngine.SceneManagement.SceneManager.LoadScene("LetsPlay");
     }
 }
