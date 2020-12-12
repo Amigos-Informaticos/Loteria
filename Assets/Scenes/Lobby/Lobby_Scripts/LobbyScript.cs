d0fdfe5a2ce8dd82c8bc78f859ac2772daf4a2de
Efrain Arenas <efrain_escuela@outlook.com>
2020-12-12 01:42:29 -0600

    Commit porque se fue el internet

--- Assets/Scenes/Lobby/Lobby_Scripts/LobbyScript.cs
+++ Assets/Scenes/Lobby/Lobby_Scripts/LobbyScript.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class LobbyScript : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI txtPlayerOne;
+    [SerializeField] private TextMeshProUGUI txtPlayerTwo;
+    [SerializeField] private TextMeshProUGUI txtPlayerThree;
+    [SerializeField] private TextMeshProUGUI txtPlayerFour;
+    [SerializeField] private TextMeshProUGUI btnLetsGo;
     void Start()
     {
-     Debug.Log("This is the lobby");   
+        this.txtPlayerOne = Localization.GetMessage();
     }
 }
