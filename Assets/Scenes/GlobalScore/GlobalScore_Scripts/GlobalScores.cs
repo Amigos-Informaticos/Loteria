0f6810051db4de21fca02f2cd2cf1798cf9f40a5
Alexis Alvarez <alexisao@hotmail.com>
2020-12-26 21:45:15 -0600

    WIP GetGlobalScore y Serialización de GlobalScore

--- Assets/Scenes/GlobalScore/GlobalScore_Scripts/GlobalScores.cs
+++ Assets/Scenes/GlobalScore/GlobalScore_Scripts/GlobalScores.cs
@@ -1,21 +1,36 @@
-﻿using TMPro;
+﻿using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+
 
 public class GlobalScores : MonoBehaviour
 {
     [SerializeField] private GameObject entryContainer;
     [SerializeField] private GameObject entryTemplate;
+    [SerializeField] private TextMeshProUGUI txtHighScores;
+    [SerializeField] private TextMeshProUGUI txtPlace;
+    [SerializeField] private TextMeshProUGUI txtScore;
+    [SerializeField] private TextMeshProUGUI txtPlayer;
+    void Start()
+    {
+        this.txtHighScores.text = Localization.GetMessage("GlobalScore", "HighScores");
+        this.txtPlace.text = Localization.GetMessage("GlobalScore", "Place");
+        this.txtScore.text = Localization.GetMessage("GlobalScore", "Score");
+        this.txtPlayer.text = Localization.GetMessage("GlobalScore", "Player");
+    }
     private void Awake()
     {
+        Dictionary<string, Dictionary<string, string>> globalScore = Player.GetGlobalScoreWithoutServer();
         entryTemplate.gameObject.SetActive(false);
-        
         float templateHeight = 40f;
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < globalScore.Count; i++)
         {
             GameObject entryTransform = Instantiate(entryTemplate, entryContainer.transform, true);
             RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
             entryRectTransform.anchoredPosition = new Vector2(0,-templateHeight * i);
             entryTransform.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = (i + 1).ToString();
+            entryTransform.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = globalScore[i.ToString()]["name"];
+            entryTransform.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = globalScore[i.ToString()]["points"];
             entryTransform.gameObject.SetActive(true);
         }
     }
