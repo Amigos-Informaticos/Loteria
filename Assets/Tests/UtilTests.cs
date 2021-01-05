c527a67bf04b8d7cddbbb2218c04751c886954c0
Alexis Alvarez <alexisao@hotmail.com>
2021-01-05 14:01:54 -0600

    Cambios en las pruebas unitarias

--- Assets/Tests/UtilTests.cs
+++ Assets/Tests/UtilTests.cs
@@ -11,18 +11,5 @@ namespace Tests
 		{
 			Debug.Log(Util.GetHashString("Alexis123"));
 		}
-
-		[Test]
-		public void Encrypt()
-		{
-			string original = "Here is some data to encrypt!";
-			using (Aes myAes = Aes.Create())
-			{
-				byte[] encrypted = Util.EncryptStringToBytes_Aes(original, myAes.Key, myAes.IV);
-				string roundtrip = Util.DecryptStringFromBytes_Aes(encrypted, myAes.Key, myAes.IV);
-				Debug.Log("Original: " + original);
-				Debug.Log("Round Trip: " + roundtrip);
-			}
-		}
 	}
 }
