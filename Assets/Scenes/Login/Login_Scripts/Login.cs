99b046b02f2a12fe984f47ae96d59789ebfb7ee6
Alexis Alvarez <alexisao@hotmail.com>
2021-01-05 12:49:32 -0600

    Cambios en el formato, manejo de respuestas en login y validaciones

--- Assets/Scenes/Login/Login_Scripts/Login.cs
+++ Assets/Scenes/Login/Login_Scripts/Login.cs
@@ -1,77 +1,87 @@
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Login : MonoBehaviour
 {
-	[SerializeField] private TextMeshProUGUI txtEmail;
-	[SerializeField] private TextMeshProUGUI txtPassword;
-	[SerializeField] private TextMeshProUGUI phEmail;
-	[SerializeField] private TextMeshProUGUI phPassword;
-	[SerializeField] private TextMeshProUGUI btnBack;
-	[SerializeField] private TextMeshProUGUI btnLogin;
-	[SerializeField] private TextMeshProUGUI txtFeedBackMessage;
-	private Command _command;
-	private TCPSocket _tcpSocket;
+    [SerializeField] private Image imgEmail;
+    [SerializeField] private Image imgPassword;
+    [SerializeField] private TextMeshProUGUI txtEmail;
+    [SerializeField] private TextMeshProUGUI txtPassword;
+    [SerializeField] private TextMeshProUGUI phEmail;
+    [SerializeField] private TextMeshProUGUI phPassword;
+    [SerializeField] private TextMeshProUGUI btnBack;
+    [SerializeField] private TextMeshProUGUI btnLogin;
+    [SerializeField] private TextMeshProUGUI txtFeedBackMessage;
+    private Command _command;
+    private TCPSocket _tcpSocket;
 
-	private void Start()
-	{
-		this.phEmail.text = Localization.GetMessage("Login", "Email");
-		this.phPassword.text = Localization.GetMessage("Login", "Password");
-		this.btnBack.text = Localization.GetMessage("Login", "Back");
-		this.btnLogin.text = Localization.GetMessage("Login", "Login");
-	}
-
-	public void LogIn()
-	{
-    Player player = new Player
+    private void Start()
     {
-      Email = Regex.Replace(this.txtEmail.text, @"[^\u0000-\u007F]+", string.Empty),
-      Password = Regex.Replace(this.txtPassword.text, @"[^\u0000-\u007F]+", string.Empty)
-    };
-	Debug.Log(player.Password);
-    string response = player.LogIn();
-    if (response.Equals("OK"))
-		{
-			if (player.GetPlayerFromServer())
-			{
-				Memory.Save("player",player);
-				UnityEngine.SceneManagement.SceneManager.LoadScene("SignedIn");	
-			}
-			else
-			{
-				txtFeedBackMessage.text = "Player Not Found";
-			}
-		} 
-		else
-		{
-			txtFeedBackMessage.text = response;
-		}
+        this.phEmail.text = Localization.GetMessage("Login", "Email");
+        this.phPassword.text = Localization.GetMessage("Login", "Password");
+        this.btnBack.text = Localization.GetMessage("Login", "Back");
+        this.btnLogin.text = Localization.GetMessage("Login", "Login");
+    }
+
+    public void LogIn()
+    {
+        Player player = new Player
+        {
+            Email = Regex.Replace(this.txtEmail.text, @"[^\u0000-\u007F]+", string.Empty),
+            Password = Regex.Replace(this.txtPassword.text, @"[^\u0000-\u007F]+", string.Empty)
+        };
+        string response = player.LogIn();
         Debug.Log(response);
-	}
+        if (EvaluateResponseLogIn(response))
+        {
+            if (player.GetPlayerFromServer())
+            {
+                Memory.Save("player", player);
+                UnityEngine.SceneManagement.SceneManager.LoadScene("SignedIn");
+            }
+        }
+    }
 
-	public void BackToMainMenu()
-	{
-		UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
-	}
+    public void BackToMainMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+    }
 
-	private void EvaluateResponseLogIn(string response)
-	{
-		switch (response)
-		{
-			case "WRONG PASSWORD":
-				this.txtFeedBackMessage.text = Localization.GetMessage("LogIn", "WrongPassword");
-				break;
-			case "EMAIL NOT REGISTERED":
-				this.txtFeedBackMessage.text =
-					Localization.GetMessage("LogIn", "EmailNotRegistered");
-				break;
-			case "ERROR":
-				this.txtFeedBackMessage.text = Localization.GetMessage("LogIn", "Error");
-				break;
-			default:
-				this.txtFeedBackMessage.text = "";
-				break;
-		}
-	}
+    private bool EvaluateResponseLogIn(string response)
+    {
+        bool isLoggedIn = false;
+        try
+        {
+            switch (response)
+            {
+                case "WRONG PASSWORD":
+                    this.txtFeedBackMessage.text = Localization.GetMessage("SignedIn", "WrongPassword");
+                    this.imgPassword.GetComponent<Image>().color = Util.GetHexColor("#ffbaba");
+                    break;
+                case "EMAIL NOT REGISTERED":
+                    this.txtFeedBackMessage.text =
+                        Localization.GetMessage("LogIn", "EmailNotRegistered");
+                    this.imgEmail.GetComponent<Image>().color = Util.GetHexColor("#ffbaba");
+                    break;
+                case "ERROR":
+                case "ERROR. TIMEOUT":
+                    this.txtFeedBackMessage.text = Localization.GetMessage("SignUp", "WrongConnection");
+                    break;
+                case "OK":
+                    isLoggedIn = true;
+                    break;
+                default:
+                    isLoggedIn = false;
+                    break;
+            }
+        }
+        catch (KeyNotFoundException)
+        {
+            this.txtFeedBackMessage.text = "ERROR";
+        }
+        return isLoggedIn;
+    }
 }
