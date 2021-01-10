a34ec92530eb5022d8fe51a12e7465c9d91479aa
Efrain Arenas <efrain_escuela@outlook.com>
2021-01-10 03:32:23 -0600

    Quitando comentarios

--- Assets/Scenes/Party/Scripts/PartyScript.cs
+++ Assets/Scenes/Party/Scripts/PartyScript.cs
@@ -27,7 +27,7 @@ public class PartyScript : MonoBehaviour
         _room = (Room) Memory.Load("room");
         _player = (Player) Memory.Load("player");
         _cardOnScreen = 0;
-        //_cards = new int[25]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25};
+        _cards = new int[25]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25};
         _cards = Board.GetSortedDeck(_room.IdRoom, _player.Email);
         GenerateBoard();
         this.txtFeedBackMessage.text = Util.PrintArrayBi(_player.Board.Pattern);
@@ -43,9 +43,9 @@ public class PartyScript : MonoBehaviour
         IEnumerator waitingForPlayers = WaitingForPlayers();
         IEnumerator chekIfKicked = CheckIfKicked();
         StartCoroutine(coroutine);
-        //StartCoroutine(chatCoroutine);
-        //StartCoroutine(waitingForPlayers);
-        //StartCoroutine(chekIfKicked);
+        StartCoroutine(chatCoroutine);
+        StartCoroutine(waitingForPlayers);
+        StartCoroutine(chekIfKicked);
     }
     private void ToggleStateChanged(Toggle toggle, bool state)
     {
