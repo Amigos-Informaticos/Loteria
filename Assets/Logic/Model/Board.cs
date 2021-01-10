3b6adf865e8cbec2c04cc30642317a09b8b19878
Efrain Arenas <efrain_escuela@outlook.com>
2021-01-10 03:54:59 -0600

    Adios bugs

--- Assets/Logic/Model/Board.cs
+++ Assets/Logic/Model/Board.cs
@@ -26,7 +26,6 @@ public class Board
 			}
 		}
 		this.GenerateRandom();
-		//this.Cards = new int[,]{{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}};
 	}
 
 	private void GenerateRandom()
