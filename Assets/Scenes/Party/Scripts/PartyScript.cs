238ae1e31ec7706e2646777a77e95c53045fc3e5
Edson Manuel Carballo Vera <edsonmanuelcarballovera@gmail.com>
2021-01-11 04:34:37 -0600

    No lea esto profe

--- Assets/Scenes/Party/Scripts/PartyScript.cs
+++ Assets/Scenes/Party/Scripts/PartyScript.cs
@@ -6,250 +6,253 @@ using UnityEngine.UI;
 
 public class PartyScript : MonoBehaviour
 {
-    [SerializeField] private Image[] board = new Image[25];
-    [SerializeField] private Toggle[] toggles = new Toggle[25];
-    [SerializeField] private Image cardToShow;
-    [SerializeField] private TextMeshProUGUI taChat;
-    [SerializeField] private TextMeshProUGUI txtChat;
-    [SerializeField] private TextMeshProUGUI[] txtPlayers = new TextMeshProUGUI[4];
-    [SerializeField] private TextMeshProUGUI txtFeedBackMessage;
-    [SerializeField] private TextMeshProUGUI txtScore;
-    private Player _player;
-    private Room _room;
-    private int[] _cards = new int[54];
-    private int _cardOnScreen;
-    private int _currentCard;
-    private bool _won;
-    private int _score;
-    private string _winner;
+	[SerializeField] private Image[] board = new Image[25];
+	[SerializeField] private Toggle[] toggles = new Toggle[25];
+	[SerializeField] private Image cardToShow;
+	[SerializeField] private TextMeshProUGUI taChat;
+	[SerializeField] private TextMeshProUGUI txtChat;
+	[SerializeField] private TextMeshProUGUI[] txtPlayers = new TextMeshProUGUI[4];
+	[SerializeField] private TextMeshProUGUI txtFeedBackMessage;
+	[SerializeField] private TextMeshProUGUI txtScore;
+	private Player _player;
+	private Room _room;
+	private int[] _cards = new int[54];
+	private int _cardOnScreen;
+	private int _currentCard;
+	private bool _won;
+	private int _score;
+	private string _winner;
 
-    void Start()
-    {
-        _room = (Room) Memory.Load("room");
-        _player = (Player) Memory.Load("player");
-        _score = 0;
-        _cardOnScreen = 0;
-        _cards = Board.GetSortedDeck(_room.IdRoom, _player.Email);
-        GenerateBoard();
-        this.txtFeedBackMessage.text = Util.PrintArrayBi(_player.Board.Pattern);
-        
-        foreach (Toggle toggle in toggles)
-        {
-            Toggle captured = toggle;
-            toggle.onValueChanged.AddListener((value) => ToggleStateChanged(captured, value));
-        }
-        
-        IEnumerator coroutine = ChangeCard(_room.Speed);
-        IEnumerator chatCoroutine = UpdateChat();
-        IEnumerator waitingForPlayers = WaitingForPlayers();
-        IEnumerator checkIfKicked = CheckIfKicked();
-        IEnumerator checkIfWinner = CheckIfWinner();
-        StartCoroutine(coroutine);
-        StartCoroutine(chatCoroutine);
-        StartCoroutine(waitingForPlayers);
-        StartCoroutine(checkIfKicked);
-        StartCoroutine(checkIfWinner);
-    }
-    private void ToggleStateChanged(Toggle toggle, bool state)
-    {
-        if(Convert.ToInt32(toggle.GetComponentInChildren<Image>().name) == _currentCard)
-        {
-            toggle.isOn = true;
-            _player.Board.Mark(_currentCard);
-            _score += 10;
-            txtScore.text = _score.ToString();
+	void Start()
+	{
+		_room = (Room) Memory.Load("room");
+		_player = (Player) Memory.Load("player");
+		_score = 0;
+		_cardOnScreen = 0;
+		_cards = Board.GetSortedDeck(_room.IdRoom, _player.Email);
+		GenerateBoard();
+		this.txtFeedBackMessage.text = Util.PrintArrayBi(_player.Board.Pattern);
 
-            if (_player.Board.IsEmpty())
-            {
-                Player.Patterns patterns = (Player.Patterns) Memory.Load("patterns");    
-                if (_player.HaveWon(patterns))
-                {
-                    Debug.Log("YA GANASTE!!!!");
-                    if (_player.NotifyWon(_room.IdRoom,_score))
-                    {
-                        _score *= 3;
-                    }
-                }
-            }
-            else
-            {
-                if (_player.HaveWon())
-                {
-                    Debug.Log("YA GANASTE!!!!");
-                    if (_player.NotifyWon(_room.IdRoom,_score))
-                    {
-                        _score *= 3;
-                    }
-                }
-            }
-        }
-        else
-        {
-            toggle.isOn = false;
-        }
-    }
-    private IEnumerator ChangeCard(float waitTime)
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(waitTime);
-            ChangeSpriteOfCard(_cards[_cardOnScreen]);
-            Debug.Log(_cardOnScreen);
-            _cardOnScreen++;
-        }
-    }
+		foreach (Toggle toggle in toggles)
+		{
+			Toggle captured = toggle;
+			toggle.onValueChanged.AddListener((value) => ToggleStateChanged(captured, value));
+		}
 
-    public IEnumerator WaitingForPlayers()
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(2.0f);
-            if (_room.GetPlayersInRoom())
-            {
-                StartPlayerList();
-                SetPlayerList();    
-            }
-            else
-            {
-                ExitParty();
-            }
-        }
-    }
+		IEnumerator coroutine = ChangeCard(_room.Speed);
+		IEnumerator chatCoroutine = UpdateChat();
+		IEnumerator waitingForPlayers = WaitingForPlayers();
+		IEnumerator checkIfKicked = CheckIfKicked();
+		IEnumerator checkIfWinner = CheckIfWinner();
+		StartCoroutine(coroutine);
+		StartCoroutine(chatCoroutine);
+		StartCoroutine(waitingForPlayers);
+		StartCoroutine(checkIfKicked);
+		StartCoroutine(checkIfWinner);
+	}
 
-    public IEnumerator CheckIfWinner()
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(2.0f);
-            _winner = _room.ThereIsAWinner();
-            if (!_winner.Equals("NO WINNER")&&!_winner.Equals("ERROR")&&!_winner.Equals("ERROR. TIMEOUT")&&!_winner.Equals("ROOM NOT FOUND"))
-            {
-                StopAllCoroutines();
-                GoEndGame();
-            }
-        }
-    }
+	private void ToggleStateChanged(Toggle toggle, bool state)
+	{
+		if (Convert.ToInt32(toggle.GetComponentInChildren<Image>().name) == _currentCard)
+		{
+			toggle.isOn = true;
+			_player.Board.Mark(_currentCard);
+			_score += 10;
+			txtScore.text = _score.ToString();
 
-    public IEnumerator CheckIfKicked()
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(2.0f);
-            if (!_player.IAmInRoom(_room.IdRoom).Equals("OK"))
-            {
-                OnClickBack();
-            }
-        }
-    }
+			if (_player.Board.IsEmpty())
+			{
+				Player.Patterns patterns = (Player.Patterns) Memory.Load("patterns");
+				if (_player.HaveWon(patterns))
+				{
+					Debug.Log("YA GANASTE!!!!");
+					if (_player.NotifyWon(_room.IdRoom, _score))
+					{
+						_score *= 3;
+					}
+				}
+			} else
+			{
+				if (_player.HaveWon())
+				{
+					Debug.Log("YA GANASTE!!!!");
+					if (_player.NotifyWon(_room.IdRoom, _score))
+					{
+						_score *= 3;
+					}
+				}
+			}
+		} else
+		{
+			toggle.isOn = false;
+		}
+	}
 
-    public void StartPlayerList()
-    {
-        for (int i = 0; i < _room.NumberPlayers; i++)
-        {
-            txtPlayers[i].text = Localization.GetMessage("Party", "NoPlayer");
-        }
-    }
+	private IEnumerator ChangeCard(float waitTime)
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(waitTime);
+			ChangeSpriteOfCard(_cards[_cardOnScreen]);
+			Debug.Log(_cardOnScreen);
+			_cardOnScreen++;
+		}
+	}
 
-    void SetPlayerList()
-    {
-        for (int i = 0; i < _room.Players.Count; i++)
-        {
-            txtPlayers[i].text = _room.Players[i].NickName;
-        }
-    }
+	public IEnumerator WaitingForPlayers()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(8.0f);
+			if (_room.GetPlayersInRoom())
+			{
+				StartPlayerList();
+				SetPlayerList();
+			} else
+			{
+				ExitParty();
+			}
+		}
+	}
 
-    private IEnumerator UpdateChat()
-    {
-        int counter = 0;
-        while (true)
-        {
-            yield return new WaitForSeconds(1.0f);
-            _room.GetMessages(_player.Email);
-            while (counter < _room.Messages.Count)
-            {
-                this.taChat.GetComponent<TextMeshProUGUI>().text =
-                    this.taChat.text + "\n" + _room.Messages[counter].Key +
-                    ": " + _room.Messages[counter].Value;
-                counter++;
-            }
-        }
-    }
+	public IEnumerator CheckIfWinner()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(2.0f);
+			_winner = _room.ThereIsAWinner();
+			if (!_winner.Equals("NO WINNER") && !_winner.Equals("ERROR") &&
+			    !_winner.Equals("ERROR. TIMEOUT") && !_winner.Equals("ROOM NOT FOUND"))
+			{
+				StopAllCoroutines();
+				GoEndGame();
+			}
+		}
+	}
 
-    public void ChangeSpriteOfCard(int index)
-    {
-        cardToShow.GetComponent<Image>().sprite = CreateSpriteOfACard(index);
-        cardToShow.GetComponent<Image>().name = index.ToString();
-        Debug.Log("changedSprite:"+cardToShow.GetComponent<Image>().name);
-        _currentCard = index;
-    }
+	public IEnumerator CheckIfKicked()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(2.0f);
+			if (!_player.IAmInRoom(_room.IdRoom).Equals("OK"))
+			{
+				OnClickBack();
+			}
+		}
+	}
 
-    public Sprite CreateSpriteOfACard(int idCard)
-    {
-        Texture2D texture = Resources.Load("Images/Cards/" + idCard) as Texture2D;
-        Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), 
-            new Vector2(0.5f, 0.5f));
-        return sprite;
-    }
+	public void StartPlayerList()
+	{
+		for (int i = 0; i < _room.NumberPlayers; i++)
+		{
+			txtPlayers[i].text = Localization.GetMessage("Party", "NoPlayer");
+		}
+	}
 
-    public void GenerateBoard()
-    {
-        int idBoardCard = 0;
-        for (int i = 0; i < 5; i++)
-        {
-            for (int j = 0; j < 5; j++)
-            {
-                board[idBoardCard].GetComponent<Image>().sprite = this.CreateSpriteOfACard(_player.Board.Cards[i, j]);
-                board[idBoardCard].GetComponent<Image>().name = _player.Board.Cards[i, j].ToString();
-                idBoardCard++;
-            }
-        }
-    }
+	void SetPlayerList()
+	{
+		for (int i = 0; i < _room.Players.Count; i++)
+		{
+			txtPlayers[i].text = _room.Players[i].NickName;
+		}
+	}
 
-    public void OnClickSendMessage()
-    {
-        Room room = (Room) Memory.Load("room");
-        string response = room.SendMessage(this.txtChat.text, (Player) Memory.Load("player"));
-        if (response.Equals("ERROR") || response.Equals("ERROR. TIMEOUT"))
-        {
-            this.taChat.GetComponent<TextMeshProUGUI>().text = this.taChat.text + "\n" + _player.NickName +
-                                                               ": " + this.txtChat.text +
-                                                               "<color = #ff0000ff> ERROR </color>";
-        }
-        this.txtChat.text = "";
-    }
+	private IEnumerator UpdateChat()
+	{
+		int counter = 0;
+		while (true)
+		{
+			yield return new WaitForSeconds(1.0f);
+			_room.GetMessages(_player.Email);
+			while (counter < _room.Messages.Count)
+			{
+				this.taChat.GetComponent<TextMeshProUGUI>().text =
+					this.taChat.text + "\n" + _room.Messages[counter].Key +
+					": " + _room.Messages[counter].Value;
+				counter++;
+			}
+		}
+	}
 
-    public void ExitParty()
-    {
-        if (_player.IsHost)
-        {
-            _player.IsHost = false;
-            Memory.Save("player",_player);
-        }
-        _room.ExitRoom(_player.Email);
-        Room room = new Room();
-        Memory.Save("room", room);
-    }
+	public void ChangeSpriteOfCard(int index)
+	{
+		cardToShow.GetComponent<Image>().sprite = CreateSpriteOfACard(index);
+		cardToShow.GetComponent<Image>().name = index.ToString();
+		Debug.Log("changedSprite:" + cardToShow.GetComponent<Image>().name);
+		_currentCard = index;
+	}
 
-    public void OnClickBack()
-    {
-        ExitParty();
-        UnityEngine.SceneManagement.SceneManager.LoadScene("LetsPlay");
-    }
+	public Sprite CreateSpriteOfACard(int idCard)
+	{
+		Texture2D texture = Resources.Load("Images/Cards/" + idCard) as Texture2D;
+		Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height),
+			new Vector2(0.5f, 0.5f));
+		return sprite;
+	}
 
-    public void OnClickKickPlayer(int index)
-    {
-        string playerToKick = txtPlayers[index].text;
-        if (!playerToKick.Equals(Localization.GetMessage("Party", "NoPlayer")))
-        {
-            _player.KickAPlayer(playerToKick, _room.IdRoom);
-        }
-    }
+	public void GenerateBoard()
+	{
+		int idBoardCard = 0;
+		for (int i = 0; i < 5; i++)
+		{
+			for (int j = 0; j < 5; j++)
+			{
+				board[idBoardCard].GetComponent<Image>().sprite =
+					this.CreateSpriteOfACard(_player.Board.Cards[i, j]);
+				board[idBoardCard].GetComponent<Image>().name =
+					_player.Board.Cards[i, j].ToString();
+				idBoardCard++;
+			}
+		}
+	}
 
-    public void GoEndGame()
-    {
-        Memory.Save("winner",_winner);
-        Memory.Save("score",_score);
-        ExitParty();
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Podium");
-    }
+	public void OnClickSendMessage()
+	{
+		Room room = (Room) Memory.Load("room");
+		string response = room.SendMessage(this.txtChat.text, (Player) Memory.Load("player"));
+		if (response.Equals("ERROR") || response.Equals("ERROR. TIMEOUT"))
+		{
+			this.taChat.GetComponent<TextMeshProUGUI>().text =
+				this.taChat.text + "\n" + _player.NickName +
+				": " + this.txtChat.text +
+				"<color = #ff0000ff> ERROR </color>";
+		}
+		this.txtChat.text = "";
+	}
+
+	public void ExitParty()
+	{
+		if (_player.IsHost)
+		{
+			_player.IsHost = false;
+			Memory.Save("player", _player);
+		}
+		_room.ExitRoom(_player.Email);
+		Room room = new Room();
+		Memory.Save("room", room);
+	}
+
+	public void OnClickBack()
+	{
+		ExitParty();
+		UnityEngine.SceneManagement.SceneManager.LoadScene("LetsPlay");
+	}
+
+	public void OnClickKickPlayer(int index)
+	{
+		string playerToKick = txtPlayers[index].text;
+		if (!playerToKick.Equals(Localization.GetMessage("Party", "NoPlayer")))
+		{
+			_player.KickAPlayer(playerToKick, _room.IdRoom);
+		}
+	}
+
+	public void GoEndGame()
+	{
+		Memory.Save("winner", _winner);
+		Memory.Save("score", _score);
+		ExitParty();
+		UnityEngine.SceneManagement.SceneManager.LoadScene("Podium");
+	}
 }
