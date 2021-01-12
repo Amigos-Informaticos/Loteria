c1fffff0d694d2456bf50112f192c0a5b8e5cef2
Efrain Arenas <efrain_escuela@outlook.com>
2021-01-12 03:13:54 -0600

    Preparados para la caída de internet

--- Assets/Scenes/Lobby/Lobby_Scripts/LobbyScript.cs
+++ Assets/Scenes/Lobby/Lobby_Scripts/LobbyScript.cs
@@ -65,7 +65,7 @@ public class LobbyScript : MonoBehaviour
 	        }
 	        else
 	        {
-		        OnClickBackToLetsPlay();
+		        feedbackMessage.text = Localization.GetMessage("Lobby", "Net error");
 	        }
         }
     }
