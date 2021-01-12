c75e5f2adf1e9f9ca08e0ddf63d2fa2b3c5a7d81
Alexis Alvarez <alexisao@hotmail.com>
2021-01-12 02:42:14 -0600

    GlobalScore estandarizado, manejo KNF y localizado

--- Assets/Scenes/GlobalScore/GlobalScore_Scripts/GlobalScores.cs
+++ Assets/Scenes/GlobalScore/GlobalScore_Scripts/GlobalScores.cs
@@ -1,46 +1,60 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
 using TMPro;
 using UnityEngine;
 
 public class GlobalScores : MonoBehaviour
 {
-    [SerializeField] private GameObject entryContainer;
-    [SerializeField] private GameObject entryTemplate;
-    [SerializeField] private TextMeshProUGUI txtHighScores;
-    [SerializeField] private TextMeshProUGUI txtPlace;
-    [SerializeField] private TextMeshProUGUI txtScore;
-    [SerializeField] private TextMeshProUGUI txtPlayer;
-    void Start()
-    {
-        this.txtHighScores.text = Localization.GetMessage("GlobalScore", "HighScores");
-        this.txtPlace.text = Localization.GetMessage("GlobalScore", "Place");
-        this.txtScore.text = Localization.GetMessage("GlobalScore", "Score");
-        this.txtPlayer.text = Localization.GetMessage("GlobalScore", "Player");
-        this.FillGlobalScore();
-    }
+	[SerializeField] private GameObject entryContainer;
+	[SerializeField] private GameObject entryTemplate;
+	[SerializeField] private TextMeshProUGUI txtHighScores;
+	[SerializeField] private TextMeshProUGUI txtPlace;
+	[SerializeField] private TextMeshProUGUI txtScore;
+	[SerializeField] private TextMeshProUGUI txtPlayer;
 
-    private void FillGlobalScore()
-    {
-        Dictionary<string, Dictionary<string, string>> globalScore = Player.GetGlobalScore();
-        if(globalScore != null)
-        {
-            entryTemplate.gameObject.SetActive(false);
-            float templateHeight = 40f;
-            for (int i = 0; i < globalScore.Count; i++)
-            {
-                GameObject entryTransform = Instantiate(entryTemplate, entryContainer.transform, true);
-                RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
-                entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * i);
-                entryTransform.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = (i + 1).ToString();
-                entryTransform.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = globalScore[i.ToString()]["points"];
-                entryTransform.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = globalScore[i.ToString()]["name"];
-                entryTransform.gameObject.SetActive(true);
-            }
-        }        
-    }
+	void Start()
+	{
+		try
+		{
+			this.txtHighScores.text = Localization.GetMessage("GlobalScore", "HighScores");
+			this.txtPlace.text = Localization.GetMessage("GlobalScore", "Place");
+			this.txtScore.text = Localization.GetMessage("GlobalScore", "Score");
+			this.txtPlayer.text = Localization.GetMessage("GlobalScore", "Player");
+		}
+		catch (SerializationException serializationException)
+		{
+			Debug.Log(serializationException);
+		}
 
-    public void BackToMenu()
-    {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("SignedIn");
-    }
+		this.FillGlobalScore();
+	}
+
+	private void FillGlobalScore()
+	{
+		Dictionary<string, Dictionary<string, string>> globalScore = Player.GetGlobalScore();
+		if (globalScore != null)
+		{
+			entryTemplate.gameObject.SetActive(false);
+			float templateHeight = 40f;
+			for (int i = 0; i < globalScore.Count; i++)
+			{
+				GameObject entryTransform =
+					Instantiate(entryTemplate, entryContainer.transform, true);
+				RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+				entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * i);
+				entryTransform.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text =
+					(i + 1).ToString();
+				entryTransform.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text =
+					globalScore[i.ToString()]["points"];
+				entryTransform.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text =
+					globalScore[i.ToString()]["name"];
+				entryTransform.gameObject.SetActive(true);
+			}
+		}
+	}
+
+	public void BackToMenu()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene("SignedIn");
+	}
 }
